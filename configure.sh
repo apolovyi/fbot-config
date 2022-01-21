3e6924c8806b3772b433c4b87d713135e8bf6809
Artem Polovyi <artem.polovyi@gmail.com>
2022-01-21 16:11:07 +0800

    fixed open trades

--- configure.sh
+++ configure.sh
@@ -1,3 +1,5 @@
 cp -r service/* /lib/systemd/system
 cp -r user_data/* /opt/freqtrade/user_data
+mkdir /opt/freqtrade/user_data/db
 cp -r strategies/* /opt/freqtrade/user_data/strategies
+sudo chown admin:admin -R /opt
