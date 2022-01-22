0c569e65a41af6314b3003b8ed9ae1289c949d87
Artem Polovyi <artem.polovyi@gmail.com>
2022-01-22 16:18:26 +0800

    updated strategies path

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 cp -r service/* /lib/systemd/system
 cp -r user_data/* /opt/freqtrade/user_data
-mkdir /opt/freqtrade/user_data/db
-cp -r strategies/* /opt/freqtrade/user_data/strategies
+mkdir -p /opt/freqtrade/user_data/db
+rm -r /opt/freqtrade/user_data/db/*
 sudo chown admin:admin -R /opt
