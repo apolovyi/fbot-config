fafba77da7361c73cf80a177ea8432bedbd91f87
Artem Polovyi <artem.polovyi@gmail.com>
2022-02-05 16:38:42 +0800

    bot setup

--- configure.sh
+++ configure.sh
@@ -2,4 +2,5 @@ cp -r service/* /lib/systemd/system
 cp -r user_data/* /opt/freqtrade/user_data
 mkdir -p /opt/freqtrade/user_data/db
 rm -rf /opt/freqtrade/user_data/db/*
+sudo systemctl daemon-reload
 #sudo chown admin:admin -R /opt
